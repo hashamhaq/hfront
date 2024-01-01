2d491f3c0f818b686e8e1390af6fd9b876fed0ff
hashamhaq <155137509+hashamhaq@users.noreply.github.com>
2024-01-01 11:54:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
-app.get('/', (req, res) => res.send('version 3111111'));
+app.get('/', (req, res) => res.send('changes made through git'));
 
 app.listen(port, () => console.log(`App 1 listening on port ${port}`));
